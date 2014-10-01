3a097970836e9342b1a88c91f830470b0072aac3
luben <luben93@gmail.com>
2014-10-01 15:04:24 +0200

    working sql con
    from jsp to remote sql

---
+++ src/main/tester.java
@@ -0,0 +1,13 @@
+package main;
+
+import DB.SQLUser;
+
+public class tester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SQLUser test= new SQLUser();
+		
+	}
+
+}
